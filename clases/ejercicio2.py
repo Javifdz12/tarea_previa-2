1b97e8ac27f71662a89cc96b2d461858779f5782
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-09-23 17:39:53 +0200

    gg

---
+++ clases/ejercicio2.py
@@ -0,0 +1,4 @@
+def comprobar(str):
+    if len(str)>=3 and len(str)<10:
+        return True
+    else: return False
---
+++ clases/ejercicio3.py
@@ -0,0 +1,28 @@
+list1=[]
+list2=[]
+list3=[]
+list4=[]
+list5=[]
+
+for i in range(0,11):
+    list1.append(i)
+
+
+for i in range(-10,1):
+    list2.append(i)
+
+
+for i in range(0,21,2):
+    list3.append(i)
+
+
+for i in range(-20,0):
+    if i%2!=0:
+        list4.append(i)
+    else:pass
+
+
+for i in range(0,51,5):
+    list5.append(i)
+
+

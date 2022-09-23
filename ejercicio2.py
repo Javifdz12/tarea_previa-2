2193ae53838aa179f077cc587c352dc4bd3c3679
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-09-23 17:39:56 +0200

    gg

--- ejercicio2.py
+++
@@ -1,5 +0,0 @@
-def comprobar(str):
-    if len(str)>=3 and len(str)<10:
-        return True
-    else: return False
-print(comprobar('hjbjhbjhbjhbhjjhbjh'))
---
+++ lanzador.py
@@ -0,0 +1,15 @@
+from clases.ejercicio2 import comprobar
+from clases.ejercicio3 import list1,list2,list3,list4,list5
+from clases.tabla import tabla,tabla1
+from clases.ejercicio1 import matriz
+
+def lanzar():
+    print(matriz)
+    print(comprobar('hjbjhbjhbjhbhjjhbjh'))
+    print(list1)
+    print(list2)
+    print(list3)
+    print(list4)
+    print(list5)
+    print(tabla1.generar())
+
